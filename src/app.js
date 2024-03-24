96dd8b27e5193f804478bd99fe6ee2264c788ace
Douglas Melendez <160060653+MrDoux@users.noreply.github.com>
2024-03-24 09:41:45 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["El Perro", "Mi Abuela", "El Cartero", "Mi Pajaro"];
+let action = ["Se Comio", "Orino", "Aplasto", "Quebro"];
+let what = ["Mi Tarea", "Mi Telefono", "El Carro", "La Tele"];
+let when = [
+  "Antes de Clase",
+  "Cuando Dormia",
+  "Mientras Hacia Ejercicio",
+  "Mientras Almorzaba",
+  "Mientras Rezaba"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function GeneradorEscusa(who, action, what, when) {
+  let nWho = Math.floor(Math.random() * who.length);
+  let nAction = Math.floor(Math.random() * action.length);
+  let nWhat = Math.floor(Math.random() * what.length);
+  let nWhen = Math.floor(Math.random() * when.length);
+  return (
+    "..." +
+    who[nWho] +
+    " " +
+    action[nAction] +
+    " " +
+    what[nWhat] +
+    " " +
+    when[nWhen]
+  );
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function onLoad() {
+  var excusa = document.getElementById("excuse");
+  excusa.innerHTML = GeneradorEscusa(who, action, what, when);
+}
+window.onload = onLoad;
